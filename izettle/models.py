c609d83133ef4c0dee64b1ba82fdd122a1a3f3e7
Alexander Holmbäck <alexande.holmback@creuna.se>
2017-12-27 20:58:58 +0100

    :-D

--- izettle/models.py
+++ izettle/models.py
@@ -5,6 +5,7 @@ class Test(models.Model):
     field = models.CharField(max_length=255)
     product = models.ForeignKey('izettle.Product', null=True)
 
+
 class Product(models.Model):
     name = models.CharField(max_length=255)
 
--- izettle/serializers.py
+++ izettle/serializers.py
@@ -2,12 +2,15 @@ from rest_framework import serializers
 from . import models
 
 
-class TestSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.Test
-        fields = '__all__'
-
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Product
+        fields = '__all__'
+
+
+class TestSerializer(serializers.ModelSerializer):
+    product = ProductSerializer(read_only=True)
+
+    class Meta:
+        model = models.Test
         fields = '__all__'
