0f2ebb344142769b3cb353f31b0265665db6c5eb
Carl <calsal@calsal-mbp.local>
2018-01-08 12:05:19 +0100

    Change context/product to return correct json response

--- izettle/views.py
+++ izettle/views.py
@@ -24,8 +24,20 @@ class SKUViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.SKUSerializer
 
 def product(request):
-    print(request)
-    product = Product.objects.filter(pk=1).values('id', 'name', 'sku', 'sku__variant__name')
+    allSKUs = Product.objects.values('id', 'name', 'sku', 'sku__variant__name', 'sku__price')
+    products = Product.objects.values('id', 'name')
+    context = []
 
-    context = list(product)
+    for product in products:
+        variantsArr = []
+        for sku in allSKUs:
+            if sku['sku'] is not None and sku['id']==product['id']:
+                variantsArr.append({'sku_id': sku['sku'], 'variant_name': sku['sku__variant__name'], 'price': sku['sku__price']})
+        if len(variantsArr) > 0:
+            context.append({'id': product['id'], 'name': product['name'], 'variants': variantsArr})
+
+    #context = list(context)
+    #print(context)
+
     return JsonResponse(context, safe=False)
+
